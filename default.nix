f52af858a7b6707311cc997f50d36f2910a0418d
Tom Dimiduk <tom@dimiduk.net>
2021-04-04 06:51:26 -0700

    add default.nix

---
+++ default.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.python3Packages.buildPythonApplication {
+  pname = "datacli";
+  src = ./.;
+  version = "0.1";
+  propagatedBuildInputs = with pkgs.python3Packages; [attrs cattrs ipython mypy black isort];
+}
