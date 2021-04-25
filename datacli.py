99ad9cb5e15ab00479afd0e5e1e090389f0d4603
Tom Dimiduk <tom@dimiduk.net>
2021-04-25 10:50:21 -0700

    add reading from config file

--- datacli.py
+++ datacli.py
@@ -4,6 +4,7 @@ import json
 import os
 from typing import Set, Type, TypeVar, List, Optional, Dict
 import collections
+import pathlib
 
 import attr
 import cattr # type: ignore
@@ -44,26 +45,58 @@ class Arguments:
                 i += 1
         return cls(positional, keyword)
 
-    def get(self, key: str) -> Optional[str]:
-        return self.keyword.get(key)
+    def get(self, key: str, default: Optional[str] = None) -> Optional[str]:
+        return self.keyword.get(key, default)
 
-def find_obj(t: Type[T], args: Arguments, prefix: List[str] = []):
+@attr.s(auto_attribs=True, frozen=True)
+class ConfigFiles:
+    configs: List[Dict[str, str]]
+
+    def get(self, key: str, default: Optional[str] = None) -> Optional[str]:
+        for config in self.configs:
+            if key in config:
+                return config[key]
+        return default
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class Source:
+    args: Arguments
+    config_files: ConfigFiles
+    from_env: bool = True
+
+    def get(self, key: str) -> Optional[str]:
+        env_value = os.environ.get(key) if self.from_env else None
+        return self.args.get(key, self.config_files.get(key, env_value))
+
+
+def load_config_files(filenames: List[str]) -> ConfigFiles:
+    # reverse the order so that later config files override earlier ones
+    def load(name):
+        if not pathlib.Path(name).exists():
+            raise ValueError("Could not find config file {name}")
+        return json.load(open(name))
+
+    return ConfigFiles([load(name) for name in filenames[::-1]])
+
+
+def find_obj(t: Type[T], source: Source, prefix: List[str] = []):
     if not attr.has(t):
         raise ValueError(f"{t} is not an attrs class")
     def find(field):
         if field.type is None:
             raise ValueError(f"Field {field.name} of {t} lacks a type annotation")
-        return find_value(field.name, field.type, args, prefix)
+        return find_value(field.name, field.type, source, prefix)
     d: dict = {field.name: find(field) for field in attr.fields(t)}
     return t(**d) # type: ignore
 
 
-def find_value(name, t: Type[T], args: Arguments, prefix: List[str] = []) -> T:
-    parts = prefix + [name]
+def find_value(name, t: Type[T], source: Source, prefix: List[str] = []) -> T:
+    prefix = prefix + [name]
     if attr.has(t):
-        return find_obj(t, args, parts)
-    prefixed_name = ".".join(parts)
-    value = args.get(prefixed_name)
+        return find_obj(t, source, prefix)
+    prefixed_name = ".".join(prefix)
+    value = source .get(prefixed_name)
     if value is None:
         value = os.environ.get(prefixed_name)
 
@@ -72,7 +105,7 @@ def find_value(name, t: Type[T], args: Arguments, prefix: List[str] = []) -> T:
     return interpret_string(value, t)
 
 
-def unused(parts: List[str], t: Type[T]) -> bool:
+def arg_unused(parts: List[str], t: Type[T]) -> bool:
     fields_dict = attr.fields_dict(t)
     if parts[0] not in fields_dict:
         return True
@@ -85,17 +118,18 @@ def unused(parts: List[str], t: Type[T]) -> bool:
 
 
 def check_unused(args: Arguments, t: Type[T]) -> List[str]:
-
-    return [arg for arg in args.keyword.keys() if unused(arg.split('.'), t)]
+    return [arg for arg in args.keyword.keys() if arg_unused(arg.split('.'), t)]
 
 
 
 def clidata(t: Type[T]) -> T:
     args = Arguments.from_argv()
+    config_files = load_config_files(args.positional)
     unknown = check_unused(args, t)
     if unknown:
         print(f"Unknown arguments: {unknown}")
         sys.exit(1)
-    return find_obj(t, args)
+    source = Source(args, config_files)
+    return find_obj(t, source)
 
 
