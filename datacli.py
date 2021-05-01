31503244f826d2f133703b2a121b537755fad9e2
Tom Dimiduk <tom@dimiduk.net>
2021-05-01 11:00:53 -0700

    Add help message

--- datacli.py
+++ datacli.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import json
 import os
-from typing import Set, Type, TypeVar, List, Optional, Dict
+from typing import Set, Type, TypeVar, List, Optional, Dict, Union
 import collections
 import pathlib
 
@@ -28,6 +28,7 @@ def interpret_string(s: str, t: Type[T]) -> T:
 class Arguments:
     positional: List[str]
     keyword: Dict[str, str]
+    help: bool = False
 
     @classmethod
     def from_argv(cls, args: List[str] = sys.argv) -> "Arguments":
@@ -36,13 +37,15 @@ class Arguments:
         positional = []
         while i < len(args):
             arg = args[i]
+            if arg in {"-h", "--help"}:
+                return Arguments([], {}, True)
             if arg[:2] == "--":
                 keyword[arg[2:]] = args[i + 1]
                 i += 2
             else:
                 positional.append(arg)
                 i += 1
-        return cls(positional, keyword)
+        return cls(positional, keyword, not (keyword or positional))
 
     def get(self, key: str, default: Optional[str] = None) -> Optional[str]:
         return self.keyword.get(key, default)
@@ -122,9 +125,21 @@ def arg_unused(parts: List[str], t: Type[T]) -> bool:
 def check_unused(args: Arguments, t: Type[T]) -> List[str]:
     return [arg for arg in args.keyword.keys() if arg_unused(arg.split("."), t)]
 
+def describe(t: Type[T]) -> Dict[str, Union[str, dict]]:
+    def desc(t):
+        if attr.has(t):
+            return describe(t)
+        return str(t.__name__)
+    return {f.name: desc(f.type) for f in attr.fields(t)}
+
 
 def clidata(t: Type[T]) -> T:
-    args = Arguments.from_argv()
+    args =  Arguments.from_argv()
+    if args.help:
+        print(f"Usage: {sys.argv[0]} [config_file] [--key: value]")
+        for key, value in describe(t).items():
+            print(f"  --{key}: {value}")
+        sys.exit(0)
     config_files = load_config_files(args.positional)
     unknown = check_unused(args, t)
     if unknown:
