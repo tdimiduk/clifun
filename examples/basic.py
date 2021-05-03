eef4d71361a7c0a880f34a22af71c1a65330e269
Tom Dimiduk <tom@dimiduk.net>
2021-05-02 19:44:54 -0700

    fix basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,5 +1,5 @@
 import attr
-import datacli
+import clattr
 
 
 @attr.s(auto_attribs=True, frozen=True)
@@ -7,11 +7,11 @@ class Basic:
     a: int
     b: str = "Optional"
 
-def my_program(data: Foo):
+def my_program(data: Basic):
     # Your actual program will go here. For this example we just print the input.
     print(data)
 
 
 if __name__ == "__main__":
-    foo = datacli.build(Foo)
-    my_program(foo)
+    data = clattr.build(Basic)
+    my_program(data)
