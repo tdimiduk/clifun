fb9ee1161622be6c8b9ba4bd245490f6507005c1
Tom Dimiduk <tom@dimiduk.net>
2022-01-03 13:16:56 -0800

    Remove the classmethods

--- src/clifun/api.py
+++ src/clifun/api.py
@@ -8,6 +8,21 @@ from . import inputs
 from . import interpret_string
 from .tools import NOT_SPECIFIED, T, get_parameters, is_optional, type_to_string
 
+def call(
+    c: Callable[..., T], args: Optional[List[str]] = None, interpret=interpret_string.interpret
+) -> T:
+    """
+    Call a function from the command line
+
+    Assembles the inputs to a function from command line arguments, environment variables, and config files and call it.
+    """
+    argv = sys.argv if args is None else args
+    source = assemble_input_sources(argv)
+    input_values = inputs.for_callable(c, interpret)
+    check_help(source, input_values)
+    check_invalid_args(source, input_values)
+    return assemble(c, collect_values(input_values, source, interpret), [])
+
 
 class Arguments:
     def __init__(
@@ -17,30 +32,29 @@ class Arguments:
         self.keyword = keyword
         self.help = help
 
-    @classmethod
-    def from_argv(cls, args: Optional[List[str]] = None) -> "Arguments":
-        if args is None:
-            args = sys.argv
-        i = 1
-        keyword = {}
-        positional = []
-        while i < len(args):
-            arg = args[i]
-            key = arg[2:]
-            if arg in {"-h", "--help"}:
-                return Arguments([], {}, True)
-            if arg[:2] == "--":
-                if len(args) < i + 2:
-                    raise ValueError(f"Missing value for argument: {key}")
-                keyword[key] = args[i + 1]
-                i += 2
-            else:
-                positional.append(arg)
-                i += 1
-        return cls(positional, keyword, not (keyword or positional))
-
     def get(self, key: str, default: Optional[str] = None) -> Optional[str]:
         return self.keyword.get(key, default)
+
+def interpret_arguments(args: Optional[List[str]] = None) -> Arguments:
+    if args is None:
+        args = sys.argv
+    i = 1
+    keyword = {}
+    positional = []
+    while i < len(args):
+        arg = args[i]
+        key = arg[2:]
+        if arg in {"-h", "--help"}:
+            return Arguments([], {}, True)
+        if arg[:2] == "--":
+            if len(args) < i + 2:
+                raise ValueError(f"Missing value for argument: {key}")
+            keyword[key] = args[i + 1]
+            i += 2
+        else:
+            positional.append(arg)
+            i += 1
+    return Arguments(positional, keyword, not (keyword or positional))
 
 
 class ConfigFiles:
@@ -56,23 +70,21 @@ class ConfigFiles:
 
 class Source:
     def __init__(
-        self, args: Arguments, config_files: ConfigFiles, from_env: bool = True
+        self, args: Arguments, config_files: ConfigFiles
     ):
         self.args = args
         self.config_files = config_files
-        self.from_env = from_env
 
     def get(self, key: str, default: Optional[T] = None) -> Union[str, T, None]:
-        env_value = os.environ.get(key.upper(), default) if self.from_env else default
+        env_value = os.environ.get(key.upper(), default)
         return self.args.get(key, self.config_files.get(key, env_value))
 
     def get_value(self, value: inputs.Value) -> Union[str, T, None]:
         return self.get(value.prefixed_name, value.default)
 
-    @classmethod
-    def from_argv(cls, args: List[str] = sys.argv) -> "Source":
-        args_object = Arguments.from_argv(args)
-        return cls(args_object, load_config_files(args_object.positional))
+def assemble_input_sources(args: List[str]) -> Source:
+    args_object = interpret_arguments(args)
+    return Source(args_object, load_config_files(args_object.positional))
 
 
 def load_config_files(filenames: List[str]) -> ConfigFiles:
@@ -158,12 +170,3 @@ def describe_needed(input_values: List[inputs.Value]) -> List[str]:
     return [desc(v) for v in input_values]
 
 
-def call(
-    c: Callable[..., T], args: Optional[List[str]] = None, interpret=interpret_string.interpret
-) -> T:
-    argv = sys.argv if args is None else args
-    source = Source.from_argv(argv)
-    input_values = inputs.for_callable(c, interpret)
-    check_help(source, input_values)
-    check_invalid_args(source, input_values)
-    return assemble(c, collect_values(input_values, source, interpret), [])
