b308d6640b914f50c3a57f598c84aecdef7a8082
Tom Dimiduk <tom@dimiduk.net>
2022-01-03 17:01:40 -0800

    add test of file reading

--- test/test_clifun.py
+++ test/test_clifun.py
@@ -6,8 +6,8 @@ import pytest
 
 import clifun
 
-sys.path.append(str(pathlib.Path(__file__).parents[1] / "examples"))
-print(sys.path)
+examples_dir = pathlib.Path(__file__).parents[1] / "examples"
+sys.path.append(str(examples_dir))
 import advanced
 import basic
 import function
@@ -49,3 +49,15 @@ def test_function():
     value = clifun.call(function.my_program, args=args)
 
     assert value == (1, "not provided")
+
+def test_config_file():
+    args = ["test_config", str(examples_dir / "foo.json"), "--c", "1"]
+
+    value = clifun.call(advanced.Bar, args)
+
+    assert value == advanced.Bar(advanced.Foo(dt.datetime(2021, 1, 1), "str"), 1)
+
+    args2 = args + ["--f.b", "test"]
+    value2 = clifun.call(advanced.Bar, args2)
+
+    assert value2 == advanced.Bar(advanced.Foo(dt.datetime(2021, 1, 1), "test"), 1)
